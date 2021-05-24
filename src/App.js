72ad0e0612145a448e5587952769703a704166d4
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-05-24 13:45:22 -0400

    useEffect add to load functions upon reload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import React, { useEffect, useState } from "react";
 export default function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat)
+    console.log("Longitude is:", long)
+  }, [lat, long]);
   
   
   

9445d8d67b4d9cf075af5667683974f926e049f6
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-05-24 13:55:08 -0400

    fetch data async call add for position

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
 export default function App() {
+
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
+    const fetchData = async () => {
     navigator.geolocation.getCurrentPosition(function(position) {
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
     });
 
-    console.log("Latitude is:", lat)
-    console.log("Longitude is:", long)
-  }, [lat, long]);
+    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+  }
+  fetchData();
+ }, [lat, long]);
+
+  
   
   
   

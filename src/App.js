6b6692c038b3e5736bf22e8cbae23f829f4c351a
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-05-25 16:05:17 -0400

    data being fetched now just have to design and style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Weather from './components/Weather';
+import Weather from './components/weather';
 
 export default function App() {
   const [lat, setLat] = useState([]);
